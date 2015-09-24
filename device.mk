cdaa6dbf64b49a01583f63e0597846a1c842d27a
sundarajan srinivasan <ssrinivasan@cyngn.com>
2015-09-24 10:18:58 -0700

    VM: rendang: tweak vm parameter
    * use xhdpi-1024 heap config
    * set hwui config to match ODM
    
    Change-Id: I67c482892f18fdda3b717e5108fbc1e9bd241196

--- device.mk
+++ device.mk
@@ -35,7 +35,11 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 TARGET_SCREEN_HEIGHT := 854
 TARGET_SCREEN_WIDTH := 480
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapminfree=6m \
+    dalvik.vm.heapstartsize=14m
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
