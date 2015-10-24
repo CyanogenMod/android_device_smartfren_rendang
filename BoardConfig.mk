b26403cd595da63507b7e398c35e05584ce9b13f
Deepak Kundra <deepakkundra@gmail.com>
2015-10-23 18:20:16 -0700

    IMS: Rendang is not supporting VT only VoLTE
    Change-Id: I8095ea7a6468c94b7a6891d124e60dac120e0406

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,6 @@ include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 DEVICE_PATH := device/smartfren/rendang
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
-# Enables CSVT
-TARGET_USES_CSVT := true
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8916_X6_6171E
 
